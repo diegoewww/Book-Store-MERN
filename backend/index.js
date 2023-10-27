7db29f752d5266d6a30750be9a898a595d4c1deb
tuvieja <105611148+n1na666@users.noreply.github.com>
2023-10-27 01:38:19 -0500

    showBookDetailsInReact

--- backend/index.js
+++ backend/index.js
@@ -7,11 +7,11 @@ import cors from 'cors'
 const app = express()
 app.use(express.json())
 app.use(cors())
-app.use(cors({
-  origin: 'http://localhost:3000',
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type']
-}))
+// app.use(cors({
+//   origin: 'http://localhost:3000',
+//   methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//   allowedHeaders: ['Content-Type']
+// }))
 app.disable('x-powered-by')
 
 app.get('/', (request, response) => {
